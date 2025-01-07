056cbc739764950a37fe614ecb6d64dbb605810a
Mohammad Mohammadi <103150443+mohammadi-com@users.noreply.github.com>
2025-01-07 11:17:34 +0000

    Refactor codebase

--- backend/utils/mail.py
+++ backend/utils/mail.py
@@ -5,7 +5,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import COMMASPACE, formatdate
 
-from envs import GMAIL_APP_PASSWORD
+from config.envs import GMAIL_APP_PASSWORD
 
 def send_mail(send_from = "noreply@example.com", send_to=["noreply@example.com"], subject="TestTailoredResume", text="This is a test email!", files=None, server="smtp.gmail.com", port=465):
     assert isinstance(send_to, list)
