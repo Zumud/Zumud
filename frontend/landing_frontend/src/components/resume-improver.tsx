18538c18322dc6ea053575fb41b456d65de5f6d2
Mohammad Mohammadi <103150443+mohammadi-com@users.noreply.github.com>
2025-04-12 12:04:44 +0100

    fix server address

--- frontend/landing_frontend/src/components/resume-improver.tsx
+++ frontend/landing_frontend/src/components/resume-improver.tsx
@@ -38,7 +38,7 @@ export default function ResumeImprover() {
 
     try {
       const xhr = new XMLHttpRequest();
-      xhr.open('POST', 'http://127.0.0.1:8000/applications/resume/improve', true);
+      xhr.open('POST', 'http://gettailoredresume.com:8000/applications/resume/improve', true);
       xhr.responseType = 'blob';
       
       xhr.onload = function() {
