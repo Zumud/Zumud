0c8d9cf29ebffc2ea08aee2a692831898fb04828
Mohammad Mohammadi <103150443+mohammadi-com@users.noreply.github.com>
2025-04-12 12:14:42 +0100

    fix CORS issue

--- frontend/landing_frontend/next.config.ts
+++ frontend/landing_frontend/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/applications/:path*',
+        destination: 'http://gettailoredresume.com:8000/applications/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
